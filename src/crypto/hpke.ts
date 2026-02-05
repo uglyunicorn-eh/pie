2a0c3a8b977197d25e3e734ef2694a83d116e044
Pavel Reznikov <pashka.reznikov@gmail.com>
2026-02-04 21:09:09 -0800

    Improve types (#5)
    * Refactor CipherContext type in hpke.ts for clarity and consistency; update exports in index.ts
    
    * Update package version to 0.1.4 in package.json

--- src/crypto/hpke.ts
+++ src/crypto/hpke.ts
@@ -9,7 +9,7 @@ import {
 } from "hpke";
 
 import { decodeBuffer, deserialize, encodeBuffer, serialize } from "../lib/utils";
-import type { CipherContext, DecipherContext, RetranslateContext } from "..";
+import type { CipherContext as CipherContextType, DecipherContext, RetranslateContext } from "..";
 
 export class HpkeEnvelopeError extends Error {}
 
@@ -17,6 +17,8 @@ export interface Envelope {
   ct: string;
   enc: string;
 }
+
+export type CipherContext = CipherContextType<Envelope>;
 
 export function createCipherSuite(): CipherSuite {
   return new CipherSuite(KEM_DHKEM_P256_HKDF_SHA256, KDF_HKDF_SHA256, AEAD_AES_128_GCM);
@@ -89,7 +91,7 @@ export async function openEnvelope<T>(
   return validated.value;
 }
 
-export function envelopeContext(opts: { out: Pick<KeyPair, "publicKey"> }): CipherContext<Envelope>;
+export function envelopeContext(opts: { out: Pick<KeyPair, "publicKey"> }): CipherContext;
 
 export function envelopeContext(opts: {
   in: Pick<KeyPair, "privateKey">;
@@ -119,7 +121,7 @@ export function envelopeContext(opts?: any) {
   if (opts?.out) {
     return {
       seal: (schema, data) => sealEnvelope(schema, data, opts.out.publicKey),
-    } as CipherContext<Envelope>;
+    } as CipherContext;
   }
 
   return undefined;
